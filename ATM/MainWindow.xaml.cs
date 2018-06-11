5a17086f14d064508581ca903140df87788522d2
Pasindu Dilanka <dilanka.pasindu@metalanka.com>
2018-06-11 08:34:34 +0530

    Adds sqlconnector library

--- ATM/MainWindow.xaml.cs
+++ ATM/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace ATM
 {
@@ -20,6 +21,7 @@ namespace ATM
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
